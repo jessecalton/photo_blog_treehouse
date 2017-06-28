3c90af15eb07e07d65facf0dc8969587238a8b23
Jesse Calton <jessecalton@gmail.com>
2017-06-27 21:12:02 -0700

    adds a service

--- src/app/entries/shared/entry.service.ts
+++ src/app/entries/shared/entry.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Entry } from './entry.model';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class EntryService {
     // our getEntries() method allows us to use Angular's HTTP service to fetch data from the server
     // it will return a promise that is an array of entries
-    getEntries() {
+
+    // We need a constructor function so we can inject the HTTP Service
+    constructor(private http: Http) {
 
     }
+    // use a colon after the parentheses and type the return type; in our case, it's an array entries
+    
+    getEntries(): Promise<Entry[]> { 
+        return this.http.get('/app/entries')
+        .toPromise()
+        .then(response => response.json().data as Entry[]);
+    }
+    // the 'as' operator sets the type of the return value
 }
