8853af6fc9a62c90229ec081c6f0988bfd6e96df
Jesse Calton <jessecalton@gmail.com>
2017-06-27 23:09:01 -0700

    adds entryService service to entry list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api'; // this so all of Angular can use the package
 import { AppComponent } from './app.component'; // importing AppComponents into AppModule
 import { EntryListComponent, EntryComponent, EntryService } from './entries';
+import { InMemoryEntryService } from './backend'; // from the backend file we created
 
 // exporting the AppModule class so we can use it in our main.ts file
 @NgModule({
+    // imports array is where all additional Angular modules are referenced
+    // rendering the modules in the imports array allows us to use them all within our application
+    // imports array only contains Angular modules, not JavaScript ones
     imports: [
         BrowserModule,
-        HttpModule
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryEntryService)
     ],
     providers: [ EntryService ],
     declarations: [AppComponent, 
--- src/app/entries/entry-list/entry-list.component.ts
+++ src/app/entries/entry-list/entry-list.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core'; //import the Component decorator from the angular core
-
+import { Component, OnInit } from '@angular/core'; //import the Component decorator from the angular core
+import { EntryService } from '../shared/entry.service'; // connecting the EntryService to the EntryListComponent
+import { Entry } from '../shared/entry.model';
 
 @Component({
     selector: 'app-entry-list', //use all lowercase letters for selector and use kebab case
@@ -8,10 +9,13 @@ import {Component} from '@angular/core'; //import the Component decorator from t
 
 })
 
-// create and export component class
-export class EntryListComponent {
-    constructor() {
+// create and export component class // we say "implements OnInit" when we want the OnInit interface
+export class EntryListComponent implements OnInit {
+    entries: Entry[];
+    constructor(private entryService: EntryService) {
+// make the injected service private or public so it gets added to the prototype and can be available on other methods inside the component
+    }
+    ngOnInit() {
 
     }
-
 }
